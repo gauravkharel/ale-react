7d576009da3fadebeb701110695f62ea86e91186
gauravkharel <kharel619@gmail.com>
2023-11-01 10:25:10 +0545

    multiple reducers and, combinedReducers

--- redux-toolkit-tutorial/redux-demo/index.js
+++ redux-toolkit-tutorial/redux-demo/index.js
@@ -1,62 +1,117 @@
-import { createStore } from 'redux'
+import { createStore, bindActionCreators, combineReducers } from "redux";
 
-const CAKE_ORDERED = 'CAKE_ORDER'
-const CAKE_RESTOCKED = 'CAKE_RESTOCKED'
+// now we have two reducers
+const CAKE_ORDERED = "CAKE_ORDER";
+const CAKE_RESTOCKED = "CAKE_RESTOCKED";
+const ICECREAM_ORDERED = "ICECREAM_ORDER";
+const ICECREAM_RESTOCKED = "ICECREAM_RESTOCKED";
 
-function orderCake(){
-  return{
+function orderCake() {
+  return {
     type: CAKE_ORDERED,
-    payload: 1
-  }
+    payload: 1,
+  };
 }
 
-function restokeCake(qty=1){
-  return{
+function orderIcecream(qty = 1) {
+  return {
+    type: ICECREAM_ORDERED,
+    payload: qty,
+  };
+}
+
+function restokeCake(qty = 1) {
+  return {
     type: CAKE_RESTOCKED,
     payload: qty,
-  }
+  };
 }
 
-const initialState ={
+function restokeIcecream(qty = 1) {
+  return {
+    type: ICECREAM_RESTOCKED,
+    payload: qty,
+  };
+}
+
+// const initialState = {
+//   numOfCakes: 10,
+//   numOfIce: 10,
+// };
+
+
+const initialCakeState = {
   numOfCakes: 10
 }
 
+const initialIcecreamState = {
+  numOfIce: 20
+}
 
-function shopReducer(state = initialState, action) {
+function cakeReducer(state = initialCakeState, action) {
   switch (action.type) {
     case CAKE_ORDERED:
-      return { 
+      return {
         ...state,
-        numOfCakes: state.numOfCakes - 1
-      }
+        numOfCakes: state.numOfCakes - 1,
+      };
     case CAKE_RESTOCKED:
-      return { 
+      return {
         ...state,
-        numOfCakes: state.numOfCakes + action.payload
-
-
-      }
+        numOfCakes: state.numOfCakes + action.payload,
+      };
     default:
-      return state
+      return state;
   }
 }
 
-// Create a Redux store holding the state of your app.
-// Its API is { subscribe, dispatch, getState }.
-let store = createStore(shopReducer)
+function iceReducer(state = initialIcecreamState, action) {
+  switch (action.type) {
+    case ICECREAM_ORDERED:
+      return {
+        ...state,
+        numOfIce: state.numOfIce - action.payload,
+      };
+    case ICECREAM_RESTOCKED:
+      return {
+        ...state,
+        numOfIce: state.numOfIce + action.payload,
+      };
+    default:
+      return state;
+  }
+}
+
+const combined = combineReducers({cakeReducer, iceReducer})
+
+let store = createStore(combined)
 
 // You can use subscribe() to update the UI in response to state changes.
 // Normally you'd use a view binding library (e.g. React Redux) rather than subscribe() directly.
 // There may be additional use cases where it's helpful to subscribe as well.
 
-const unsubscribe = store.subscribe(() => console.log('update store',store.getState()))
+const unsubscribe = store.subscribe(() =>
+  console.log("update store", store.getState())
+);
 
 // The only way to mutate the internal state is to dispatch an action.
 // The actions can be serialized, logged or stored and later replayed.
-store.dispatch(orderCake())
-store.dispatch(orderCake())
-store.dispatch(orderCake())
-store.dispatch(restokeCake(10))
+// store.dispatch(orderCake())
+// store.dispatch(orderCake())
+// store.dispatch(orderCake())
+// store.dispatch(restokeCake(10))
 
-unsubscribe()
+//bindActionCreator just bind the actions with dispact function
+//which is not really necessary as of now but, used to be useful back in the days.
+const action = bindActionCreators(
+  { orderCake, restokeCake, orderIcecream, restokeIcecream },
+  store.dispatch
+);
+action.orderCake();
+action.orderCake();
+action.orderCake();
+action.restokeCake(3);
+action.orderIcecream(10);
+action.restokeIcecream(43);
 
+unsubscribe();
