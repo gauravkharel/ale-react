3984bd25419d0ec7578b1d1f5c9729cb44a221a6
gauravkharel <kharel619@gmail.com>
2023-11-01 09:52:54 +0545

    redux cakeshop example

--- redux-toolkit-tutorial/redux-demo/index.js
+++ redux-toolkit-tutorial/redux-demo/index.js
@@ -1,23 +1,41 @@
 import { createStore } from 'redux'
 
-/**
- * This is a reducer - a function that takes a current state value and an
- * action object describing "what happened", and returns a new state value.
- * A reducer's function signature is: (state, action) => newState
- *
- * The Redux state should contain only plain JS objects, arrays, and primitives.
- * The root state value is usually an object. It's important that you should
- * not mutate the state object, but return a new object if the state changes.
- *
- * You can use any conditional logic you want in a reducer. In this example,
- * we use a switch statement, but it's not required.
- */
-function counterReducer(state = { value: 0 }, action) {
+const CAKE_ORDERED = 'CAKE_ORDER'
+const CAKE_RESTOCKED = 'CAKE_RESTOCKED'
+
+function orderCake(){
+  return{
+    type: CAKE_ORDERED,
+    payload: 1
+  }
+}
+
+function restokeCake(qty=1){
+  return{
+    type: CAKE_RESTOCKED,
+    payload: qty,
+  }
+}
+
+const initialState ={
+  numOfCakes: 10
+}
+
+
+function shopReducer(state = initialState, action) {
   switch (action.type) {
-    case 'counter/incremented':
-      return { value: state.value + 1 }
-    case 'counter/decremented':
-      return { value: state.value - 1 }
+    case CAKE_ORDERED:
+      return { 
+        ...state,
+        numOfCakes: state.numOfCakes - 1
+      }
+    case CAKE_RESTOCKED:
+      return { 
+        ...state,
+        numOfCakes: state.numOfCakes + action.payload
+
+
+      }
     default:
       return state
   }
@@ -25,19 +43,20 @@ function counterReducer(state = { value: 0 }, action) {
 
 // Create a Redux store holding the state of your app.
 // Its API is { subscribe, dispatch, getState }.
-let store = createStore(counterReducer)
+let store = createStore(shopReducer)
 
 // You can use subscribe() to update the UI in response to state changes.
 // Normally you'd use a view binding library (e.g. React Redux) rather than subscribe() directly.
 // There may be additional use cases where it's helpful to subscribe as well.
 
-store.subscribe(() => console.log(store.getState()))
+const unsubscribe = store.subscribe(() => console.log('update store',store.getState()))
 
 // The only way to mutate the internal state is to dispatch an action.
 // The actions can be serialized, logged or stored and later replayed.
-store.dispatch({ type: 'counter/incremented' })
-// {value: 1}
-store.dispatch({ type: 'counter/incremented' })
-// {value: 2}
-store.dispatch({ type: 'counter/decremented' })
-// {value: 1}
+store.dispatch(orderCake())
+store.dispatch(orderCake())
+store.dispatch(orderCake())
+store.dispatch(restokeCake(10))
+
+unsubscribe()
+
