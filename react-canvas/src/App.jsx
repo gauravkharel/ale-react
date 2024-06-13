fa0f89e843bb442c18a797e7def1f22af6b19e63
gauravkharel <kharel619@gmail.com>
2024-06-13 23:02:17 +0545

    useCanvas hook

--- react-canvas/src/App.jsx
+++ react-canvas/src/App.jsx
@@ -10,8 +10,15 @@ function App() {
     const delta  = count % 800;
     context.fillRect(10+ delta, 10, 100, 100);
   };
+ const sketch = (context, count) => {
+    context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+    context.fillStyle = "pink";
+    const delta  = count % 700;
+    context.fillRect(10+ delta, 10, 100, 100);
+  };
   return <>
-    <Canvas draw={draw} width="800" height="500" />
+    <Canvas draw={draw} style={{border: '10px solid black'}} width="800" height="500" />
+    <Canvas draw={sketch} style={{border: '10px solid black'}} width="100" height="100" />
   </>
 }
 
--- react-canvas/src/component/Canvas.jsx
+++ react-canvas/src/component/Canvas.jsx
@@ -1,25 +1,9 @@
-import { useEffect, useRef } from "react";
+import useCanvas from "../useCanvas";
 
 const Canvas = (props) => {
   const { draw, ...rest } = props;
-  const ref = useRef();
-
-  useEffect(() => {
-    const canvas = ref.current;
-    const context = canvas.getContext("2d");
-    let count = 0;
-    let animationId;
-
-    const renderer = () => {
-      count++;
-      draw(context, count);
-      animationId = window.requestAnimationFrame(renderer);
-    };
-    renderer();
-
-    return () => window.cancelAnimationFrame(animationId);
-  }, [draw]);
-
+  const ref = useCanvas(draw);
+  
   return <canvas ref={ref} {...rest} />;
 };
 
